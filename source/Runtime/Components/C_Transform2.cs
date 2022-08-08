d64c1e873e0ea559a5c7d2fa961ab56057a88790
sahmed19 <ssahmed@usc.edu>
2022-08-08 03:04:33 -0700

    editing c+transform

--- source/Runtime/Components/C_Transform2.cs
+++ source/Runtime/Components/C_Transform2.cs
@@ -1,8 +1,23 @@
+using Microsoft.Xna.Framework;
 
 namespace ProjectMono.ECS.Components {
-
     public class C_Transform2 {
+        public Vector2 Position { get; private set; }
+        public Vector2 Scale { get; private set; }
+        public float Angle {get; private set; }
         
+//CONSTRUCTORS
+        public C_Transform2() : this(Vector2.Zero) {}
+        public C_Transform2(Vector2 position) : this(position, 0.0f) {}
+        public C_Transform2(Vector2 position, float angle) : this(position, angle, Vector2.One) {}
+        public C_Transform2(Vector2 position, float angle, Vector2 scale) {
+            Position = position;
+            Angle = angle;
+            Scale = scale;
+        }
+        public void SetPosition(Vector2 position) => Position = position;
+        public void AddPosition(Vector2 deltaPos) => Position += deltaPos;
+        public void SetAngle(float angle) => Angle = angle;
+        public void AddAngle(float deltaAngle) => Angle += deltaAngle;
     }
-
 }
